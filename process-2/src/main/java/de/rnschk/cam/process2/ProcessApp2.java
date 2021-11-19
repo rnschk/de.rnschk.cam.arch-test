ecbb324c2618345e4073442024dceaa22feb3bfb
rnschk <rnschk@gmail.com>
2021-11-19 22:29:38 +0100

    EnableProcessApplication

--- process-2/src/main/java/de/rnschk/cam/process2/ProcessApp2.java
+++ process-2/src/main/java/de/rnschk/cam/process2/ProcessApp2.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableProcessApplication
+@EnableProcessApplication("process-app-2")
 public class ProcessApp2 {
 
 	public static void main(String[] args) {
